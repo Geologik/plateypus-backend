d2638e71150b5ffe429f31257dbe5875e7b5379a
Kristian Thy <thy@42.dk>
2018-10-25 00:00:59 +0200

    Add echo method

--- plateypus/backend.py
+++ plateypus/backend.py
@@ -1,13 +1,21 @@
 """This is the Plateypus backend."""
 
-from flask import Flask
+from flask import Flask, jsonify, request
 PLATEYPUS = Flask(__name__)
 
 
 @PLATEYPUS.route('/')
 def hello():
-    """The essential minimum."""
-    return u'Hello world!'
+    """Return Hello world! json."""
+    resp = jsonify({'Hello': 'world!'})
+    resp.headers['Cache-Control'] = 'no-cache'
+    return resp
+
+
+@PLATEYPUS.route('/echo', methods=['POST'])
+def echo():
+    """Return whatever was posted."""
+    return jsonify(request.json)
 
 
 if __name__ == '__main__':
