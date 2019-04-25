80e546de1190e725fa2017bc842fc6da81759164
Kristian Thy <krt@sigmaestimates.com>
2019-04-26 01:13:56 +0200

    Migrate to Elasticsearch

--- plateypus/etl/etl_utils.py
+++ plateypus/etl/etl_utils.py
@@ -2,17 +2,16 @@
 
 from datetime import datetime
 
+from dateutil.parser import parse as parse_date
+from elasticsearch_dsl import Search
 from ftputil import FTPHost
 from ftputil.error import FTPOSError
-from sqlalchemy.orm import sessionmaker
 
-from plateypus.backend import DB
-from plateypus.models import Metadata
+from plateypus.models import INDEX_METADATA
 
 
 def ftp_connect(server, user, passwd, cwd):
-    """Connect to an FTP server, change working dir
-    and return FTPHost object."""
+    """Connect to an FTP server, change working dir and return FTPHost object."""
     try:
         ftp = FTPHost(server, user, passwd)
         ftp.chdir(cwd)
@@ -22,19 +21,18 @@ def ftp_connect(server, user, passwd, cwd):
 
 
 def ls_lt(ftp):
-    """Return directory listing from FTP connection,
-    sorted by time modified."""
+    """Return directory listing from FTP connection, sorted by time modified."""
     files = [(ftp.stat(f), f) for f in ftp.listdir(ftp.curdir)]
     files.sort(key=lambda fil: fil[0].st_mtime)
     return files
 
 
-def newer_than_latest(country, timestamp):
-    """Check whether the given timestamp is newer
-    than the last downloaded dump for country."""
-    with sessionmaker(bind=DB.engine)() as session:
-        last_updated = (
-            session.query(Metadata).filter_by(country=country).first().last_updated
-            or datetime.min
-        )
-        return timestamp > last_updated
+def newer_than_latest(elastic, country, timestamp):
+    """Check whether the given timestamp is newer than the last downloaded dump for country."""
+    last_updated = datetime.min
+
+    search = Search(using=elastic, index=INDEX_METADATA).filter("term", country=country)
+    if search.count() > 0:
+        last_updated = parse_date(search.execute()[0].last_updated)
+
+    return timestamp > last_updated
